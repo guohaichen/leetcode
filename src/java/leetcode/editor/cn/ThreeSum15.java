f5ed811a8da6188c4929ec9f345e6ca2961aefad
guohai <1299117865@qq.com>
2024-05-29 17:39:11 +0800

    3数之和，将1，23个数作为两个数，然后再双指针

---
+++ src/java/leetcode/editor/cn/ThreeSum15.java
@@ -0,0 +1,103 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，
+ * 同时还满足 nums[i] + nums[j] + nums[k] == 0 。请
+ * <p>
+ * 你返回所有和为 0 且不重复的三元组。
+ * <p>
+ * 注意：答案中不可以包含重复的三元组。
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：nums = [-1,0,1,2,-1,-4]
+ * 输出：[[-1,-1,2],[-1,0,1]]
+ * 解释：
+ * nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0 。
+ * nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0 。
+ * nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0 。
+ * 不同的三元组是 [-1,0,1] 和 [-1,-1,2] 。
+ * 注意，输出的顺序和三元组的顺序并不重要。
+ * <p>
+ * <p>
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：nums = [0,1,1]
+ * 输出：[]
+ * 解释：唯一可能的三元组和不为 0 。
+ * <p>
+ * <p>
+ * 示例 3：
+ * <p>
+ * <p>
+ * 输入：nums = [0,0,0]
+ * 输出：[[0,0,0]]
+ * 解释：唯一可能的三元组和为 0 。
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * 3 <= nums.length <= 3000
+ * -10⁵ <= nums[i] <= 10⁵
+ * <p>
+ * <p>
+ * Related Topics 数组 双指针 排序 👍 6890 👎 0
+ */
+public class ThreeSum15 {
+    public static void main(String[] args) {
+        Solution solution = new ThreeSum15().new Solution();
+        solution.threeSum(new int[]{-1, 0, 1, 2, -1, -4});
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<List<Integer>> threeSum(int[] nums) {
+            List<List<Integer>> result = new ArrayList<>();
+            Arrays.sort(nums);
+            //可以试着将三数之和的问题转变为2数之和
+            for (int i = 0; i < nums.length - 2; i++) {
+                if (i > 0 && nums[i] == nums[i - 1]) {
+                    continue;
+                }
+                if (nums[i] > 0) {
+                    break;
+                }
+                int left = i + 1;
+                int right = nums.length - 1;
+                while (left < right) {
+                    if ((nums[left] + nums[right]) + nums[i] < 0) {
+                        left++;
+                    } else if ((nums[left] + nums[right]) + nums[i] > 0) {
+                        right--;
+                    } else {
+                        result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                        //判断一下已经装入过 result的组合，因为数组是排序过的，所以相等的话是相邻的,要排除也就是去重。可能有多个，需使用while而不是if
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        //放置后需要各移动一位
+                        left++;
+                        right--;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
